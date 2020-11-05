efa39c543cf669f20fb42b67c62f64ba756976da
deep-sarkar <deepsarkar96@gmail.com>
2020-11-05 12:17:12 +0530

    [Deep]Add.added items table

--- create_table.py
+++ create_table.py
@@ -5,7 +5,9 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 create_table = 'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)'
+cursor.execute(create_table)
 
+create_table = 'CREATE TABLE IF NOT EXISTS items(name text, price reql)'
 cursor.execute(create_table)
 
 connection.commit()

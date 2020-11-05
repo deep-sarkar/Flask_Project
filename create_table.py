5a4f9c0969ea6f158bea0264c056cc1b21b60da8
deep-sarkar <deepsarkar96@gmail.com>
2020-11-05 20:53:41 +0530

    [Deep]Add.added primary key field in items table

--- create_table.py
+++ create_table.py
@@ -7,8 +7,11 @@ cursor = connection.cursor()
 create_table = 'CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT)'
 cursor.execute(create_table)
 
-create_table = 'CREATE TABLE IF NOT EXISTS items(name text, price reql)'
+create_table = 'CREATE TABLE IF NOT EXISTS items(id INTEGER PRIMARY KEY,name text, price reql)'
 cursor.execute(create_table)
+
+# for row in cursor.execute("select * from items"):
+#     print(row)
 
 connection.commit()
 connection.close()
